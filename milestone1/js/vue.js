e921fa749550525c61054f0214ff6211fb35cb75
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-12 13:19:27 +0100

    milestone 1

--- milestone1/js/vue.js
+++ milestone1/js/vue.js
@@ -8,7 +8,33 @@ Stampare all’interno di una lista, un item per ogni todo.
 Se la proprietà `done` è uguale a `true`, visualizzare il testo del todo ~~sbarrato~~./** */
 
 const app = new Vue ({
-  
+  el: '#app',
+
+  data:{
+    toDoList: [
+      {
+        text: 'comprare cibo gatti',
+        done: false
+      },
+      {
+        text: 'chiamare marilù',
+        done: false
+      },
+      {
+        text: 'ritirare zaino',
+        done: true
+      },
+      {
+        text: 'fare torta',
+        done: false
+      }
+    ],
+
+
+
+
+
+  }
 
 
 
